2747831ba3880a0da9a64f783056b1fa94b1631e
ridho9 <p.ridho9@gmail.com>
2025-05-09 09:54:30 +0700

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module project
+
+go 1.24.2
+
+require (
+	connectrpc.com/connect v1.18.1
+	connectrpc.com/vanguard v0.3.0
+	google.golang.org/genproto/googleapis/api v0.0.0-20250505200425-f936aa4a68b2
+	google.golang.org/protobuf v1.36.6
+)
+
+require google.golang.org/genproto/googleapis/rpc v0.0.0-20250428153025-10db94c68c34 // indirect
---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"project/gen/flightprice/v1/flightpricev1connect"
+	"project/service/flightprice"
+	"syscall"
+	"time"
+
+	"connectrpc.com/vanguard"
+)
+
+//go:generate buf generate
+func main() {
+	flightPriceService := flightprice.NewService()
+
+	mux := http.NewServeMux()
+
+	vanguardServices := []*vanguard.Service{
+		vanguard.NewService(flightpricev1connect.NewFlightServiceHandler(flightPriceService)),
+	}
+	transcoder, err := vanguard.NewTranscoder(
+		vanguardServices,
+		vanguard.WithUnknownHandler(http.NotFoundHandler()),
+	)
+	if err != nil {
+		log.Fatalf("Failed to create Vanguard transcoder: %v", err)
+	}
+	mux.Handle("/", transcoder)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	// rest is server starting and grace stop code
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("HTTP server listening on %s", server.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("Could not listen on %s: %v\n", server.Addr, err)
+		}
+	}()
+
+	<-stopChan
+	log.Println("Shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server shutdown failed: %v", err)
+	}
+	log.Println("Server gracefully stopped")
+}
